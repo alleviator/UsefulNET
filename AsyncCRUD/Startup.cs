8f72d0ad297d7db6b5993cb6d05d5cbe92e76840
Max Savchenko <m.a.savchenko@gmail.com>
2015-07-16 11:12:11 +0300

    Initial blank MVC Template

---
+++ AsyncCRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AsyncCRUD.Startup))]
+namespace AsyncCRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
